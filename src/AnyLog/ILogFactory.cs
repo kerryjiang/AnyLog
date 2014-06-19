58e01f0daed8e384aea274f23f3504fcdd67594a
Kerry Jiang <kerry-jiang@hotmail.com>
2014-06-19 23:37:01 +0800

    basic interfaces

---
+++ src/AnyLog/ILogFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AnyLog
+{
+    /// <summary>
+    /// LogFactory Interface
+    /// </summary>
+    public interface ILogFactory
+    {
+        /// <summary>
+        /// Gets the log by name.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
+        ILog GetLog(string name);
+    }
+}
