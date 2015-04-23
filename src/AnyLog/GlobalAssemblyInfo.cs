4c86e2bf80ee5b27a8724aecfdc6fb06958b73cb
Kerry Jiang <kerry-jiang@hotmail.com>
2015-04-24 00:50:47 +0800

    improved the nuspec files

--- src/AnyLog/GlobalAssemblyInfo.cs
+++ src/AnyLog/GlobalAssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 
-[assembly: AssemblyCompany("AnyLog")]
+[assembly: AssemblyCompany("Kerry Jiang")]
 [assembly: AssemblyProduct("AnyLog")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyVersion("0.1.0.0")]
