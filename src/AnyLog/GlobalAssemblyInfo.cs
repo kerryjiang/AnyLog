af93370be440f629729504c57eaa6b435c7359bb
Kerry Jiang <kerry-jiang@hotmail.com>
2015-04-23 22:49:34 +0800

    reorganized assemblyInfo classes

---
+++ src/AnyLog/GlobalAssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyCompany("AnyLog")]
+[assembly: AssemblyProduct("AnyLog")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
